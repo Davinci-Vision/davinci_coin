03cbd6554e17e1fdf0db63c78059a1d2def0600e
147indiana <147indiana@gmail.com>
2018-07-30 06:47:27 +0900

    created new genesis block

--- dac_mainnet/params/config.go
+++ dac_mainnet/params/config.go
@@ -25,7 +25,7 @@ import (
 
 // Genesis hashes to enforce below configs on.
 var (
-	MainnetGenesisHash = common.HexToHash("0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3")
+	MainnetGenesisHash = common.HexToHash("0x6e6614e400cf51388dd9859a4d5d8c043624a0e7ec5640cb71b463f8b2074e1a")
 	TestnetGenesisHash = common.HexToHash("0x41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d")
 )
 
